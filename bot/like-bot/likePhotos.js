70894ccfe5a2752fa92396f1ecd8041a6270e0cf
Venglas <bartek.bober97@gmail.com>
2019-09-22 12:43:44 +0200

    add randomly generated wait time

--- bot/like-bot/likePhotos.js
+++ bot/like-bot/likePhotos.js
@@ -6,18 +6,15 @@ const likePhotos = async (photos) => {
 		await photo.click();
 		await ig.page.waitFor(1000);
 
-		// await ig.page.waitForXPath('//section/span/button/span[contains(@aria-label, "Like")]');
-
-		// const likeButton = ig.page.$x('//section/span/button/span[contains(@aria-label, "Like")]');
-
 		// if likeBot element exist it's means that this photo was not liked yet
-
 		if ((await ig.page.$('section span button span[aria-label="Like"]')) !== null) {
-			await ig.page.click('section span button span[aria-label="Like"]');
+			await ig.page.click('section span button span[aria-label="Like"]'); // like photo
 			await ig.page.waitFor(2000);
 
-			await ig.page.click('.ckWGn'); // close photo modal (div[role=dialog] button:nth-child(3))
-			await ig.page.waitFor(3000); //here should be a randomly generated number of seconds to wait
+			await ig.page.click('.ckWGn'); // close photo modal
+
+			const waitTime = Math.floor(Math.random() * (8000 - 1500 + 1)) + 1500;
+			await ig.page.waitFor(waitTime);
 		} else if ((await ig.page.$x('//section/span/button/span[contains(@aria-label, "Unlike")]')) !== null) {
 			await ig.page.waitFor(2000);
 			await ig.page.click('.ckWGn');
