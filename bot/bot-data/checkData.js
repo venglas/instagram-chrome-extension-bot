567d9065366b9592f2c904e6be62c2c324547c7d
Venglas <bartek.bober97@gmail.com>
2019-09-24 18:59:15 +0200

    add checking files function

---
+++ bot/bot-data/checkData.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const consoleLineBreak = require('../helpers/consoleLineBreak');
+
+const checkData = async () => {
+	if ((await fs.existsSync('bot/bot-data/followedUsers.json')) === false) {
+		console.log('Create followedUsers.json file');
+		consoleLineBreak('=');
+
+		await fs.writeFileSync('bot/bot-data/followedUsers.json', JSON.stringify({ followedUsers: [] }));
+	}
+
+	if ((await fs.existsSync('bot/bot-data/profileInfo.json')) === false) {
+		console.log('Create profileInfo.json file');
+		consoleLineBreak('=');
+
+		await fs.writeFileSync('bot/bot-data/profileInfo.json', JSON.stringify({ username: '', posts: 0, followers: 0, following: 0, date: '', allLikes: 0, lastLikes: 0 }));
+	}
+};
+
+module.exports = checkData;
--- bot/helpers/openInstagram.js
+++ bot/helpers/openInstagram.js
@@ -4,8 +4,10 @@ const login = require('../login');
 const config = require('../config');
 const closeNotificationModal = require('../closeNotificationModal');
 const updateProfileInfo = require('../updateProfileInfo');
+const checkData = require('../bot-data/checkData');
 
 const openInstagram = async () => {
+	await checkData();
 	await ig.init(); // open browser
 	await setLanguage(); // set language to en
 	await login(config.username, config.password); //login to instagram
