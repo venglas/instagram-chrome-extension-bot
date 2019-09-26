4eb1da0429928dbc838aaec6de2ad120d21f5ba9
Venglas <bartek.bober97@gmail.com>
2019-09-26 10:27:58 +0200

    fix bot stops at unfollowing when user was blocked on instagram

--- bot/follow-bot/unfollow.js
+++ bot/follow-bot/unfollow.js
@@ -5,7 +5,7 @@ const ig = require('../instagram');
 
 const unfollow = async () => {
 	if (config.unfollow.isStart === true) {
-		const followed = JSON.parse(fs.readFileSync('bot/bot-data/followedUsers.json'));
+		const followedUsers = JSON.parse(fs.readFileSync('bot/bot-data/followedUsers.json'));
 		const dateNow = Date.now(); // now time in miliseconds
 		const hour = 3600000; // hour calculate to miliseconds
 		const day = 86400000; // day calculate to miliseconds
@@ -16,27 +16,37 @@ const unfollow = async () => {
 
 		await openInstagram();
 
-		for (user of followed.followedUsers) {
+		for (user of followedUsers) {
 			const followedUserTime = dateNow - user.FollowDate; // time in milisecond since followed this user
 
 			if (followedUserTime / day > config.unfollow.afterDays) {
 				await ig.page.goto(`${ig.BASE_URL}/${user.userName}`);
 				await ig.page.waitFor(2000);
 
-				const unfollowButton1 = await ig.page.$x("//button[contains(text(), 'Following')]"); // click for open modal for unfollowing
-				await unfollowButton1[0].click();
+				const isUserBlocked = await ig.page.$x("//button[contains(text(), 'Following')]");
 
-				await ig.page.waitForXPath('//button[contains(text(), "Unfollow")]'); // wait for unfollow modal
-				const unfollowButton2 = await ig.page.$x('//button[contains(text(), "Unfollow")]');
-				await unfollowButton2[0].click(); // unfollow user
+				// if bot not find 'following' button that user are not followed or user was blocked
+				if (isUserBlocked.length === 0) {
+					console.log(`ERROR THIS USER (${user.userName}) WAS BLOCKED ON INSTAGRAM`);
+					await ig.page.goto(ig.BASE_URL);
 
-				console.log(`User -> ${user.userName} unfollowed.`);
+					//add here romove blocked user from followed list
+				} else {
+					const unfollowButton1 = await ig.page.$x("//button[contains(text(), 'Following')]"); // click for open modal for unfollowing
+					await unfollowButton1[0].click();
 
-				followed.followedUsers.splice(i, 1); //remove unfollowed use
+					await ig.page.waitForXPath('//button[contains(text(), "Unfollow")]'); // wait for unfollow modal
+					const unfollowButton2 = await ig.page.$x('//button[contains(text(), "Unfollow")]');
+					await unfollowButton2[0].click(); // unfollow user
 
-				await fs.writeFileSync('bot/bot-data/followedUsers.json', JSON.stringify(followed.followedUsers)); // save data without unfollowed users
+					console.log(`User -> ${user.userName} unfollowed.`);
 
-				await ig.page.waitFor(1000);
+					followedUsers.splice(i, 1); //remove unfollowed use
+
+					await fs.writeFileSync('bot/bot-data/followedUsers.json', JSON.stringify(followedUsers)); // save data without unfollowed users
+
+					await ig.page.waitFor(1000);
+				}
 			}
 			i++;
 		}
